c3183e305fa92be3a9b2ce89f12f0f366cd67794
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-10 22:33:47 +0900

    validate flag

--- cli/main.go
+++ cli/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	textPtr := flag.String("text", "", "Text to parse.")
-	metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};.")
+	textPtr := flag.String("text", "", "Text to parse. (Required)")
+	metricPtr := flag.String("metric", "chars", "Metric {chars|words|lines};. (Required)")
 	uniquePtr := flag.Bool("unique", false, "Measure unique values of a metric.")
 	flag.Parse()
+
+	if *textPtr == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n", *textPtr, *metricPtr, *uniquePtr)
 }
